98bbb81f504110b3d1ad293fd931ae3166112905
sonny faristrianto <sonnyf4r15@gmail.com>
2024-05-12 14:36:33 +0700

    tambah semua file

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import MenuMakanan from "./Page/MenuMakanan";
+
+function App() {
+  return (
+    <div>
+      <MenuMakanan />
+    </div>
+  );
+}
+
+export default App;
